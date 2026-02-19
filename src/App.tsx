f9c347c5f2e82251e9c11644e2fde175ab8ff7a4
maimuna-t-333 <maimuna.tabr@gmail.com>
2026-02-20 01:26:07 +0600

    Increment decrement in another way

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
 import { useCounter } from './store'
+import { OtherComponent } from './OtherComponent';
 
 const App = () => {
-const {count,increment,decrement}= useCounter();
+// const {count,increment,decrement}= useCounter();
+
+const {count}= useCounter();
   return (
     <div>
       <h1>Count: {count}</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      {/* <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button> */}
+      <OtherComponent/>
     </div>
   )
 }
